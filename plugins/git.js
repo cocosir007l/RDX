2c7d5b3fe7cb1687ff0072aa77f36c9b68b79848
cocosir007l <98014231+cocosir007l@users.noreply.github.com>
2022-02-17 21:12:56 +0530

    Update git.js

--- plugins/git.js
+++ plugins/git.js
@@ -7,23 +7,23 @@ const Lang = Language.getString('wallpaper');
 
 Asena.addCommand({pattern: 'git', fromMe: false, desc: "Gives github link of the bot"}, (async (message, match) => {
 
-    var skl = await axios.get("https://telegra.ph/file/991a06ce548b9bf7d5185.jpg", { responseType: 'arraybuffer' })
+    var skl = await axios.get("https://imgur.com/a/ag1k15Z", { responseType: 'arraybuffer' })
 
     await message.sendMessage(Buffer(skl.data), MessageType.image, {mimetype: Mimetype.png, caption: `RDX BOT CREATED BY ABHIRAM SACHU 
 
-🌐CREATOR NUMBER : wa.me/18254138025
+💕CREATOR NUMBER : wa.me/919072460920
 
-🌐WEBSITE LINK : https://bit.ly/3rnlWcu
+💖WEBSITE LINK : https://bit.ly/3rnlWcu
 
-🌐GIT LINK : https://bit.ly/348VMRP
+💖GIT LINK : https://github.com/cocosir007l/RDX
 
-🌐 GROUP LINK : https://bit.ly/3Heg6PY
+💓 GROUP LINK : https://bit.ly/3Heg6PY
 
-🌐 AND THANKS TO MY  COLLABORATORS 🥰
+💓 AND THANKS TO MY  COLLABORATORS 🥰
 
-🌐THANKS TO SUPPORT 😊🥰
+💖THANKS TO SUPPORT 😊🥰
 
-🌐RDX BOT🌐
+💖ᴄͥᴏͭᴄᷤᴏ sͫᴇͤʀ  ͢  ᴍᴏᴅs💓
 `}) 
 
 }));
